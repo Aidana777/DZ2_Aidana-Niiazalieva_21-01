ad293f67015af80ebdda284d87cd0849dcc39e72
Aidana777 <aidananiazalieva27@gmail.com>
2023-01-09 18:49:03 +0600

    first commit

--- app.js
+++ app.js
@@ -5,10 +5,21 @@ if (traffic === 'красный') {
 } else if (traffic === 'желтый') {
     alert("Подождите пожалуйста");
 } else if (traffic === 'зелёный') {
-    alert("Можете идти");
+    alert("Перейти дорогу");
 } else {
     alert(" Кроме Выше перечисленых цветов Вы не можете Ввести другие цвета");
 }
-for (let i=20; i >= 0; i--){
+
+for (let i = 20; i >= 0; i--) {
     console.log(i);
+}
+
+// 1
+let arr = ['10', '20', '30', '50', '235', '3000'];
+for (let item of arr) {
+    let items = item.toString()
+    if (items[0] === '1' || items[0] === '2' || items[0] === '5') {
+        console.log(item);
+
+    }
 }
